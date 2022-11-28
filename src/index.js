0ac46d378d4f0c8a2acaeb0af341289e4ff71990
Anastasiia <guba.anastaysha@gmail.com>
2022-11-28 15:29:54 +0200

    deleted persist gate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './index.css';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import App from 'components/App';
 import { theme } from './constants/theme';
 
@@ -14,9 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
